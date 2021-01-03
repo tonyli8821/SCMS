da2b9188cc40c52450f9456800ea22ef01d3968a
tonyli <375465265@qq.com>
2021-01-03 16:48:11 +0800

    2

--- src/com/tonyli8821/dao/model/TbSpinfo.java
+++ src/com/tonyli8821/dao/model/TbSpinfo.java
@@ -1,4 +1,19 @@
 package com.tonyli8821.dao.model;
 
 public class TbSpinfo implements java.io.Serializable{
+	private String id;	//
+	private String spname;		//
+	private String jc;
+	private String cd;
+	private String dw;
+	private String gg;
+	private String bz;
+	private String ph;
+	private String pzwh;
+	private String memo;
+	private String gysname;
+	
+	public TbSpinfo(){
+	}
+	public 
 }
